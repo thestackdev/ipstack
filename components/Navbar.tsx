16fd7e8209f3d7ec9902576d3bc0540faa5fde6f
thestackdev <shanmukeshwar03@gmail.com>
2023-09-23 10:53:33 +0530

    updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "@/app/theme-toggle";
 import { cn } from "@/lib/utils";
-import { Terminal } from "lucide-react";
+import { Github, Terminal } from "lucide-react";
+import Link from "next/link";
 
 export default function Navbar({
   className,
@@ -17,10 +18,15 @@ export default function Navbar({
       <span className="flex gap-2 justify-center items-center">
         <Terminal className="w-6 h-6" />
         <h1 className="text-xl font-semibold tracking-tight transition-colors">
-          Codefusionz
+          IP Stack
         </h1>
       </span>
-      <ThemeToggle />
+      <div className="flex items-center justify-center gap-2">
+        <Link href="https://github.com/thestackdev/ipstack" target="_blank">
+          <Github className="w-6 h-6" />
+        </Link>
+        <ThemeToggle />
+      </div>
     </nav>
   );
 }
